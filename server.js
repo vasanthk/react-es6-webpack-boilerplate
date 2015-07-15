116db92e46712990d238258b8a00247d5519651c
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-07-15 12:01:27 -0700

    Added History API fallback option

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
-  hot: true
+  hot: true,
+  historyApiFallback: true
 }).listen(5000, 'localhost', function (err) {
     if (err) {
       console.log(err);
