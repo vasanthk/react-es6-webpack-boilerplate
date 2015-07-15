6e137a3ab24320b9cee30cd1fefcbfc7bc1fcc59
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-07-15 11:54:48 -0700

    Added config notes

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return <h1>Hello, World!</h1>;
+    return (
+      // Add your component markup/subcomponent references here.
+      <h1>Hello, World!</h1>
+    );
   }
 }
 
