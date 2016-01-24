4acc34d43eb811161a006f6da6d630112e9bbeb2
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-01-24 09:20:06 -0800

    Revert "Fix dis version"
    This reverts commit 90eeeb02a165259a29b66f8e42ef8d141b580235.

--- scripts/App.js
+++ scripts/App.js
@@ -1,10 +1,7 @@
 import React, {Component} from 'react';
-import InlineEdit from 'react-inline-edit';
 
 export default class App extends Component {
   render() {
-    console.log('Inline Edit', InlineEdit);
-    console.log('React', React);
     return (
       // Add your component markup and other subcomponent references here.
       <h1>Hello, World!</h1>
